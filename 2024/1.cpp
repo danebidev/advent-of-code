f31ec9c20f8b03f88fb15511189f6b82ee73e865
danebidev <57720273+danebidev@users.noreply.github.com>
2024-12-02 15:55:12 +0100

    [Day 1] Added more benchmarking

--- 2024/1.cpp
+++ 2024/1.cpp
@@ -2,10 +2,13 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <numeric>
 #include <unordered_map>
 #include <vector>
 
 #define INPUT_SIZE 1000
+#define WARMUP_RUNS 10
+#define BENCHMARK_RUNS 100
 
 using namespace std;
 
@@ -40,12 +43,37 @@ int part2(const vector<int>& a, const vector<int>& b) {
     return ans;
 }
 
+template <typename Func>
+void benchmark(Func func, const vector<int>& a, const vector<int>& b, const string& part_name) {
+    vector<long long> us_times;
+    vector<long long> ns_times;
+    us_times.reserve(BENCHMARK_RUNS);
+    ns_times.reserve(BENCHMARK_RUNS);
+
+    for (int i = 0; i < WARMUP_RUNS; ++i) {
+        func(a, b);
+    }
+
+    int last_ans = 0;
+    for (int i = 0; i < BENCHMARK_RUNS; ++i) {
+        const auto begin = chrono::high_resolution_clock::now();
+        last_ans = func(a, b);
+        const auto end = chrono::high_resolution_clock::now();
+
+        us_times.push_back(chrono::duration_cast<chrono::microseconds>(end - begin).count());
+        ns_times.push_back(chrono::duration_cast<chrono::nanoseconds>(end - begin).count());
+    }
+
+    long long mean_us_time = accumulate(us_times.begin(), us_times.end(), 0LL) / us_times.size();
+    long long mean_ns_time = accumulate(ns_times.begin(), ns_times.end(), 0LL) / ns_times.size();
+
+    cout << part_name << endl;
+    cout << "    Last Answer: " << last_ans << endl;
+    cout << "    Average Time: " << mean_us_time << " µs | " << mean_ns_time << " ns" << endl;
+}
+
 int main() {
     ifstream input("input");
-    if (!input) {
-        cerr << "Error opening input file" << endl;
-        return 1;
-    }
 
     vector<int> a, b;
     int n1, n2;
@@ -58,19 +86,9 @@ int main() {
         b.emplace_back(n2);
     }
 
-    auto benchmark = [&](auto func, const string& part_name) {
-        const auto begin = chrono::high_resolution_clock::now();
-        int ans = func(a, b);
-        const auto end = chrono::high_resolution_clock::now();
+    // Run benchmarks
+    benchmark(part1, a, b, "Part 1");
+    benchmark(part2, a, b, "Part 2");
 
-        const auto microsecs = chrono::duration_cast<chrono::microseconds>(end - begin).count();
-        const auto nanosecs = chrono::duration_cast<chrono::nanoseconds>(end - begin).count();
-
-        cout << part_name << endl;
-        cout << "    Answer: " << ans << endl;
-        cout << "    Time: " << microsecs << " µs | " << nanosecs << " ns" << endl;
-    };
-
-    benchmark(part1, "Part 1");
-    benchmark(part2, "Part 2");
+    return 0;
 }
